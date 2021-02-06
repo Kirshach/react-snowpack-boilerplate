32d43420dd9492104cab1b9a4c05c9db4f699abe
Kirshach <kirshach@mail.ru>
2021-02-06 19:13:13 +0300

    bracket spacing removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App.jsx';
@@ -7,7 +7,7 @@ ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 if (import.meta.hot) {
