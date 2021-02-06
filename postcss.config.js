5787d5f2ee3cf490f2ad6656e76ef7d12703a4c7
Kirshach <kirshach@mail.ru>
2021-02-06 19:10:13 +0300

    CommonJS allowed for .js files

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,9 @@
-const cssnano = require("cssnano");
-const postcssNested = require('postcss-nested'),
-const postcssPresetEnv = require("postcss-preset-env");
-const stylelint = require("stylelint");
-const postcssBemLinter = require("postcss-bem-linter"); 
+const cssnano = require('cssnano');
+const postcssNested = require('postcss-nested');
+const postcssPresetEnv = require('postcss-preset-env');
+const stylelint = require('stylelint');
+const postcssBemLinter = require('postcss-bem-linter');
 
 module.exports = {
-  plugins: [
-    stylelint(),
-    postcssBemLinter(),
-
-    postcssNested(),
-    postcssPresetEnv(),
-    cssnano(),
-  ],
+  plugins: [stylelint(), postcssBemLinter(), postcssNested(), postcssPresetEnv(), cssnano()],
 };
